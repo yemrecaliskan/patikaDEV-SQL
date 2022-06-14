4790c1ab1c6797e853f3fe0b2a4d3f2623f30734
Yunus Emre Caliskan <emrecaliskaan@gmail.com>
2022-06-14 14:18:27 +0300

    ödev 9 yüklendi

---
+++ odev-9.sql
@@ -0,0 +1,8 @@
+-- city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT city.city, country.country FROM city JOIN country ON city.country_id = country.country_id;
+
+-- customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT payment.payment_id, customer.first_name FROM payment JOIN customer ON payment.customer_id = customer.customer_id;
+
+-- customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+SELECT customer.first_name, customer.last_name,rental.rental_id FROM customer JOIN rental ON rental.customer_id = customer.customer_id;
